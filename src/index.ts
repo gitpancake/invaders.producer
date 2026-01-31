df73adf8f5b7f538bede7ac59d54a89131866df4
henry <13052648+gitpancake@users.noreply.github.com>
2026-01-31 09:13:57 -0800

    Remove NestJS health API in favor of Prometheus

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { config } from "dotenv";
 import { CombinedSyncCron } from "./util/cron-jobs/combined-sync";
-import { startHealthApi } from "./util/health-api";
 
 config({ path: ".env" });
 
 const main = async () => {
-    // Start the health API server
-    await startHealthApi();
-
-    // Start the cron jobs
     const combinedSyncCron = new CombinedSyncCron("*/5 * * * *");
 
     combinedSyncCron.register();
